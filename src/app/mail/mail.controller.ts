660bca30724165293c39704d8275fa09af5d80fa
BenSuzarte <ben.dantassuzarte@gmail.com>
2024-07-03 21:26:30 -0300

    feat: add enum to set mail's status and add validator/pipes to require and response the empty values

--- src/app/mail/mail.controller.ts
+++ src/app/mail/mail.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { SaveMailDto } from './dto/save-email.dto';
 
-@Controller('api/v1/mail')
+@Controller('api/v1/mails')
 export class MailController {
 
   constructor ( private readonly mailService: MailService ) {}
